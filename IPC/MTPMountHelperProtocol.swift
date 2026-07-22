42644cb83433029fca1277d4efc707af7e9ada43
X1nto <tornike.khintibidze2@gmail.com>
2026-07-22 15:57:11 +0400

    Fix incorrect mount helper bundle names

--- IPC/MTPMountHelperProtocol.swift
+++ IPC/MTPMountHelperProtocol.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-let MTPMountHelperLabel = "dev.xinto.mtpfs.mtphelperd"
-let MTPMountHelperMachServiceName = "85342F9WTR.dev.xinto.mtpfs.mtphelperd"
+let MTPMountHelperLabel = "dev.xinto.mtpfs.mtpmounterd"
+let MTPMountHelperMachServiceName = "85342F9WTR.dev.xinto.mtpfs.mtpmounterd"
 
 @objc protocol MTPMountHelperProtocol {
     /// - Parameter reply: Path to the created mountpoint
